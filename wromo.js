363ffce5c12462599e96e23448bec6036d815241
Marketplace Wromo Studio <info.wromo@gmail.com>
2023-10-29 15:04:07 +0100

    add new

--- wromo.js
+++ wromo.js
@@ -1,4 +1,19 @@
+// wromo.js
 const { program } = require('commander');
+const { Argument } = require('./lib/argument.js');
+const { Command } = require('./lib/command.js');
+const { CommanderError, InvalidArgumentError } = require('./lib/error.js');
+const { Help } = require('./lib/help.js');
+const { Option } = require('./lib/option.js');
+
+module.exports = {
+    Argument,
+    Command,
+    CommanderError,
+    InvalidArgumentError,
+    Help,
+    Option
+  };
 
 program
   .version('1.0.0')
